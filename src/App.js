97dc75607d348133bf492678b9e288a91160e397
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-01-08 02:29:43 +0800

    Setup materialise-css into our app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect }from 'react';
+
+// Always import the .min version
+import "materialize-css/dist/css/materialize.min.css";
+import M from 'materialize-css/dist/js/materialize.min.js';
+
 import './App.css';
 
 const App = () => {
+  useEffect( () => {
+    // Initialise Materialise JS at the start of the app
+    // Now we would be able to use Modals and all the Materialise stuff that requires JS
+    M.AutoInit();
+  });
+
   return (
     <div className="App">
     My App
