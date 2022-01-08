62a58758fb5105c98f8b57d14ae3883a1fc88fd5
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-01-08 21:30:27 +0800

    Update Preloader.js

--- src/components/loading/Preloader.js
+++ src/components/loading/Preloader.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Preloader = () => {
-    return (
-        <div className='progress teal lighten-3'>
-            <div class="indeterminate"></div>
-        </div>
-    )
-}
+  return (
+    <div className="container">
+      <div className="progress teal lighten-3">
+        <div class="indeterminate"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Preloader
+export default Preloader;
