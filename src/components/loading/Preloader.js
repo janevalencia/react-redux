0a10dc88b3517e64cb279f4d00abed195a081de7
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-01-08 21:24:20 +0800

    Create Preloader.js
    Mimicking the fetching data progress / loading bar

---
+++ src/components/loading/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Preloader = () => {
+    return (
+        <div className='progress teal lighten-3'>
+            <div class="indeterminate"></div>
+        </div>
+    )
+}
+
+export default Preloader
